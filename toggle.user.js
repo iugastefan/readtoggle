c431cacc8422666be57931af5de1f43cab4f7be6
Iuga Stefan <iugastefan@outlook.com>
2019-07-09 12:54:22 +0300

    Added some more links and changed es6 js to old js

--- toggle.user.js
+++ toggle.user.js
@@ -1,7 +1,8 @@
 // ==UserScript==
 // @name         toggle
-// @namespace    http://tampermonkey.net/
-// @version      0.1
+// @namespace    https://github.com/iugastefan
+// @homepage     https://github.com/iugastefan/readtoggle/raw/master/toggle.user.js
+// @version      0.2
 // @description  Toggles chapter's read state
 // @author       Iuga Stefan
 // @match        https://mangadex.org/title/*
@@ -10,27 +11,30 @@
 
 (function() {
     'use strict';
-    let but = document.createElement("div");
-    let sp = document.createElement("span");
-    sp.className="fas fa-eye fa-fw";
+    var but = document.createElement("div");
+    var sp = document.createElement("span");
+    sp.className = "fas fa-eye fa-fw";
     but.appendChild(sp);
     var t = document.createTextNode(" Mark all chapters read/unread");
     but.appendChild(t);
-    but.style.cursor="pointer";
-    but.onclick=()=>{
-        let x = document.getElementsByClassName("chapter_mark_read_button");
-        if(x.length!==0){
-            Array.from(x).forEach(item=>item.click());
-        }else {
+    but.style.cursor = "pointer";
+    but.onclick = function() {
+        var x = document.getElementsByClassName("chapter_mark_read_button");
+        if (x.length !== 0) {
+            Array.from(x).forEach(function(item) {
+                item.click();
+            });
+        } else {
             x = document.getElementsByClassName("chapter_mark_unread_button");
-            if(x.length!==0){
-                Array.from(x).forEach(item=>item.click());
-            }
-            else{
+            if (x.length !== 0) {
+                Array.from(x).forEach(function(item) {
+                    item.click();
+                });
+            } else {
                 console.log("nothing to click");
             }
         }
-    }
+    };
 
     document.getElementById("navbarSupportedContent").appendChild(but);
 })();
